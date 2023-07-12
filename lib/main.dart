fa996507e3be2c2634be7b16136f66f3cea8eacf
yuto90 <solidusgako4087@gmail.com>
2023-07-12 09:05:56 +0900

    Consumerで画面を部分更新

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,79 +24,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends StatelessWidget {
   @override
-  // ConsumerWidgetを継承する時refを引数で受け取る
-  // refにアクセスする事で定義したProviderにアクセスが可能
-  Widget build(BuildContext context, WidgetRef ref) {
-    // watchで定義したProviderの値を監視
-    final int counter = ref.watch(counterProvider);
-
-    final List<Todo> todoList = ref.watch(todoListProvider);
-
-    void _incrementCounter() {
-      // notifierでデータを更新
-      final notifier = ref.read(counterProvider.notifier);
-      notifier.state++;
-    }
-
-    void add() {
-      // notifierでデータを更新
-      final notifier = ref.read(counterProvider.notifier);
-      notifier.state++;
-    }
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // watchで変更を監視
-        title: Text(ref.watch(titleProvider)),
+        title: Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          return Text(ref.watch(titleProvider));
+        }),
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                counter.toString(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              todoList.isEmpty
-                  ? const Text("Todoが無いよ")
-                  : ListView(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: [
-                        for (final todo in todoList)
-                          ListTile(
-                            title: Text(todo.title),
-                            trailing: IconButton(
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.red,
-                              ),
-                              onPressed: () {
-                                ref
-                                    .read(todoListProvider.notifier)
-                                    .removeTodo(todo.id);
-                              },
-                            ),
+              Consumer(builder:
+                  (BuildContext context, WidgetRef ref, Widget? child) {
+                return Text(
+                  ref.watch(counterProvider).toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              }),
+              Consumer(builder:
+                  (BuildContext context, WidgetRef ref, Widget? child) {
+                final List<Todo> todoList = ref.watch(todoListProvider);
+                return ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    for (final todo in todoList)
+                      ListTile(
+                        title: Text(todo.title),
+                        trailing: IconButton(
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
                           ),
-                      ],
-                    ),
-              ElevatedButton(
-                onPressed: () => add(),
-                child: const Text("Todoを追加"),
-              )
+                          onPressed: () {
+                            ref
+                                .read(todoListProvider.notifier)
+                                .removeTodo(todo.id);
+                          },
+                        ),
+                      ),
+                  ],
+                );
+              }),
+              Consumer(builder:
+                  (BuildContext context, WidgetRef ref, Widget? child) {
+                return ElevatedButton(
+                  onPressed: () => ref.read(todoListProvider.notifier).addTodo(
+                        Todo(id: "10", title: "メモ10"),
+                      ),
+                  child: const Text("Todoを追加"),
+                );
+              })
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+        return FloatingActionButton(
+          onPressed: () =>
+              ref.read(counterProvider.notifier).incrementCounter(),
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        );
+      }),
     );
   }
 }
--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -3,15 +3,10 @@ import 'package:flutter_state_verify/model/todo.dart';
 
 // todoを管理する用
 class TodoListNotifier extends StateNotifier<List<Todo>> {
-  TodoListNotifier()
-      : super([
-          Todo(id: '0', title: 'メモ0'),
-          Todo(id: '1', title: 'メモ1'),
-          Todo(id: '2', title: 'メモ2'),
-        ]);
+  TodoListNotifier() : super([]);
 
   void addTodo(Todo todo) {
-    // stateに値が格納されている
+    // stateに値の参照が格納されている
     state = [...state, todo];
   }
 
@@ -20,10 +15,22 @@ class TodoListNotifier extends StateNotifier<List<Todo>> {
   }
 }
 
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier() : super(0);
+
+  void incrementCounter() {
+    state++;
+  }
+}
+
 // 定義したProvider(グローバル)
 // StateProviderは「enumや数値」などシンプルなステートを管理するのに適している
 final titleProvider = StateProvider((ref) => "タイトル");
-final counterProvider = StateProvider((ref) => 0);
 
+// todo
 final todoListProvider = StateNotifierProvider<TodoListNotifier, List<Todo>>(
     (ref) => TodoListNotifier());
+
+// カウンター
+final counterProvider =
+    StateNotifierProvider<CounterNotifier, int>((ref) => CounterNotifier());
